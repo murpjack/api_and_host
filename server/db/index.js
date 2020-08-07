2c5d6a5e2da805e160a8ec6385348fbb1e317924
Jack <jackmurphy756@gmail.com>
2020-08-07 11:56:37 +0100

    add comments

--- server/db/index.js
+++ server/db/index.js
@@ -7,6 +7,7 @@ const d = require("require-dir-all")("../data", { recursive: true });
 const url = "mongodb://localhost:27017/planner_db";
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
+// Connect to MongoDB
 mongoose.connect(url, options).catch((e) => console.error(e.message));
 
 // Clear collections first, whilst on local server
@@ -14,12 +15,11 @@ m.dining.deleteMany({}, (rej) => rej && console.error("remove dining"));
 m.leisure.deleteMany({}, (rej) => rej && console.error("remove activities"));
 m.advice.deleteMany({}, (rej) => rej && console.error("remove advice"));
 m.plan.deleteMany({}, (rej) => rej && console.error("remove plans"));
+// Add initial data to collections
 m.dining.insertMany(d.venues.dining, (rej) => rej);
 m.leisure.insertMany(d.venues.leisure, (rej) => rej);
 m.advice.insertMany(d.advice.advice, (rej) => rej);
 m.plan.insertMany(d.itineraries.itineraries, (rej) => rej);
-
-// Add initial data to collections
 
 const db = mongoose.connection;
 module.exports = db;
