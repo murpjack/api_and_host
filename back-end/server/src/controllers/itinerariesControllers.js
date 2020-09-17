0380b5a9ca7e9d857c5493df4d8807f0eb0aa642
Jack <jackmurphy756@gmail.com>
2020-09-17 11:49:27 +0100

    migrate routes and controllers into ./back-end

---
+++ back-end/server/src/controllers/itinerariesControllers.js
@@ -0,0 +1,114 @@
+const Plan = require("../models/selection");
+
+const createPlan = (req, res) => {
+  const body = req.body;
+  console.log("res", res);
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a plan",
+    });
+  }
+
+  const plan = new Plan(body);
+  if (!plan) {
+    // TODO: handle error response
+    return res.status(400).json({ success: false, error: "err" });
+  }
+
+  plan
+    .save()
+    .then(() =>
+      res.status(201).json({
+        success: true,
+        id: plan._id,
+        message: "Plan created!",
+      })
+    )
+    .catch((error) =>
+      res.status(400).json({
+        error,
+        message: "Plan not created!",
+      })
+    );
+};
+
+const updatePlan = async (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a body to update",
+    });
+  }
+
+  Plan.findOne({ _id: req.params.id }, (err, plan) => {
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: "Plan not found!",
+      });
+    }
+    plan._id = body._id;
+    plan.name = body.name;
+    plan.description = body.description;
+    plan.approvalStatus = body.approvalStatus;
+    plan.priceRating = body.priceRating;
+    plan.diningID = body.diningID;
+    plan.diner = body.diner;
+    plan.activityID = body.activityID;
+    plan.adviceID = body.adviceID;
+    plan.createdBy = body.createdBy;
+
+    plan
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: plan._id,
+          message: "Plan updated!",
+        });
+      })
+      .catch((error) => {
+        return res.status(404).json({
+          error,
+          message: "Plan not updated!",
+        });
+      });
+  });
+};
+
+const deletePlan = async (req, res) => {
+  await Plan.findOneAndDelete({ _id: req.params.id }, (err, plan) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
+    if (!plan) {
+      return res.status(404).json({ success: false, error: `Plan not found` });
+    }
+
+    return res.status(200).json({ success: true, data: plan });
+  }).catch((err) => console.log(err));
+};
+
+const getPlans = async (req, res) => {
+  await Plan.find({}, (err, plans) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+    if (!plans.length) {
+      return res.status(404).json({ success: false, error: `Plan not found` });
+    }
+    return res.status(200).json({ success: true, data: plans });
+  }).catch((err) => console.log(err));
+};
+
+module.exports = {
+  createPlan,
+  updatePlan,
+  deletePlan,
+  getPlans,
+};
--- back-end/server/src/routes/index.ts
+++ back-end/server/src/routes/index.ts
@@ -3,14 +3,56 @@ import * as controllers from "../controllers";
 
 export const router = express.Router();
 
+
+const adviceControllers = require("../controllers/adviceControllers");
+const diningControllers = require("../controllers/diningControllers");
+const planControllers = require("../controllers/itinerariesControllers");
+const leisureControllers = require("../controllers/leisureControllers");
+
 // This is a test route!!
 // router.get('/test', async (req, res) => {
 //   res.json({message: 'pass!'})
 // });
 
+/**
+ * Routes for product users.
+ */
 router.get("/user/getallusers", controllers.getAllUsers);
 router.post("/user/addsingleuser", controllers.createUser);
 router.put("/user/updatesingleuser", controllers.updateUser);
 router.delete("/user/removesingleuser", controllers.deleteUser);
+
+
+/**
+ * Routes for advice/wisdom.
+ */
+router.get("/advice/getallwisdom", adviceControllers.getWisdom);
+router.post("/advice/addwisdom", adviceControllers.createWisdom);
+router.put("/advice/updatewisdom/:id", adviceControllers.updateWisdom);
+router.delete("/advice/removewisdom/:id", adviceControllers.deleteWisdom);
+
+/**
+ * Routes for dining.
+ */
+router.get("/dining/getallvenues", diningControllers.getDiners);
+router.post("/dining/addvenue", diningControllers.createDiner);
+router.put("/dining/updatevenuedetail/:id", diningControllers.updateDiner);
+router.delete("/dining/removevenue/:id", diningControllers.deleteDiner);
+
+/**
+ * Routes for itineraries.
+ */
+router.get("/plan/getallitineraries", planControllers.getPlans);
+router.post("/plan/additinerary", planControllers.createPlan);
+router.put("/plan/updatitinerary/:id", planControllers.updatePlan);
+router.delete("/plan/removeitinerary/:id", planControllers.deletePlan);
+
+/**
+ * Routes for activities.
+ */
+router.get("/leisure/getallvenues", leisureControllers.getActivities);
+router.post("/leisure/addvenue", leisureControllers.createActivity);
+router.put("/leisure/updatevenuedetail/:id", leisureControllers.updateActivity);
+router.delete("/leisure/removevenue/:id", leisureControllers.deleteActivity);
 
 export default router;
